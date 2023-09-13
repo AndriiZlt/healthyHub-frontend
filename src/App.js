9509e992e9eec63d09b2d1ec950dc91771c63ffc
AndriiZlt <andreyzlot@gmail.com>
2023-09-13 23:18:40 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import React, { useState, useEffect } from "react";
-import { Routes, Route } from "react-router-dom";
-import Layout from "./components/Layout/Layout";
-import Login from "./components/Login/Login";
-import Home from "./components/Home/Home";
-import Dashboard from "./components/Dashboard/Dashboard";
-import Diary from "./components/Diary/Diary";
-import RecommendedFood from "./components/RecommendedFood/RecommendedFood";
+import React, { useState, useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout/Layout';
+import Login from './components/Login/Login';
+import Home from './components/Home/Home';
+import Dashboard from './components/Dashboard/Dashboard';
+import Diary from './components/Diary/Diary';
+import RecommendedFood from './components/RecommendedFood/RecommendedFood';
 
 function App() {
-  const [data, setData] = useState("");
+  const [data, setData] = useState('');
 
   useEffect(() => {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((res) => setData(res.message));
+    fetch('/api')
+      .then(res => res.json())
+      .then(res => setData(res.message));
   }, []);
 
   return (
     <>
       <Routes>
-        <Route path="/" element={<Layout />}>
+        <Route path="/healthyHub-frontend" element={<Layout />}>
           <Route index element={<Login />} />
           <Route path="home" element={<Home />} />
           <Route path="dashboard" element={<Dashboard />} />
