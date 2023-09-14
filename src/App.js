9611cf3e6ddbf60a64fa4ae3e55e264770ca4da7
AndriiZlt <andreyzlot@gmail.com>
2023-09-14 17:02:32 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout/Layout';
 import Login from './components/Login/Login';
