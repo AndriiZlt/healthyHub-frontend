b4ea07975252d5a148260fc7a5637c3a173b53b5
Evgeniy <yaremenko.x@gmail.com>
2023-09-16 00:44:21 +0300

    add desktop & tablet

--- src/components/UserData/UserGoal.jsx
+++ src/components/UserData/UserGoal.jsx
@@ -21,44 +21,48 @@ const UserGoal = () => {
   return (
     <div className={css.goalContainer}>
       <img alt="Summer hiking" src={Image} className={css.goalImg} />
-      <h2 className={css.goalTitle}>Your goal</h2>
-      <p className={css.goalText}>
-        Choose a goal so that we can help you effectively
-      </p>
-      <form className={css.goalForm}>
-        <label className={css.goalFormText}>
-          <input
-            className={css.goalFormRadio}
-            type="radio"
-            name="goal"
-            value={Goal.FIRST}
-            defaultChecked
-          />
-          <span className={css.customCheked}></span>
-          Lose Fat
-        </label>
-        <label className={css.goalFormText}>
-          <input
-            className={css.goalFormRadio}
-            type="radio"
-            name="goal"
-            value={Goal.SECOND}
-          />
-          <span className={css.customCheked}></span>
-          Maintain
-        </label>
-        <label className={css.goalFormText}>
-          <input
-            className={css.goalFormRadio}
-            type="radio"
-            name="goal"
-            value={Goal.SECOND}
-          />
-          <span className={css.customCheked}></span>
-          Gain Muscle
-        </label>
-        <button className={css.goalButton}>Next</button>
-      </form>
+      <div className={css.goalInfoContainer}>
+        <h2 className={css.goalTitle}>Your goal</h2>
+        <p className={css.goalText}>
+          Choose a goal so that we can help you effectively
+        </p>
+        <form className={css.goalForm}>
+          <label className={css.goalFormText}>
+            <input
+              className={css.goalFormRadio}
+              type="radio"
+              name="goal"
+              value={Goal.FIRST}
+              defaultChecked
+            />
+            <span className={css.customCheked}></span>
+            Lose Fat
+          </label>
+          <label className={css.goalFormText}>
+            <input
+              className={css.goalFormRadio}
+              type="radio"
+              name="goal"
+              value={Goal.SECOND}
+            />
+            <span className={css.customCheked}></span>
+            Maintain
+          </label>
+          <label className={css.goalFormText}>
+            <input
+              className={css.goalFormRadio}
+              type="radio"
+              name="goal"
+              value={Goal.SECOND}
+            />
+            <span className={css.customCheked}></span>
+            Gain Muscle
+          </label>
+          <button type="button" className={css.goalButton}>
+            Next
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
