d45df336d515f636bda9ac7a22b1296c3f87a3e6
Evgeniy <yaremenko.x@gmail.com>
2023-09-16 23:33:05 +0300

    finished page 3

--- src/components/UserData/UserBody.jsx
+++ src/components/UserData/UserBody.jsx
@@ -29,44 +29,46 @@ const UserBody = () => {
   return (
     <div className={css.bodyContainer}>
       <img alt="Body parameters" src={Image} className={css.bodyImg} />
-      <h2 className={css.bodyTitle}>Body parameters</h2>
-      <p className={css.bodyText}>
-        Enter your parameters for correct performance tracking
-      </p>
-      <form className={css.bodyForm} onSubmit={formSubmit}>
-        <label htmlFor="user_height" className={css.bodyFormText}>
-          Height
-        </label>
-        <input
-          className={css.bodyFormInput}
-          id="user_height"
-          type="number"
-          name="height"
-          value={height}
-          onChange={handleChangeHeight}
-          placeholder="Enter your height"
-        ></input>
-        <label htmlFor="user_weight" className={css.bodyFormText}>
-          Weight
-        </label>
-        <input
-          className={css.bodyFormInput}
-          id="user_weight"
-          type="number"
-          name="weight"
-          value={weight}
-          onChange={handleChangeWeight}
-          placeholder="Enter your weight"
-        ></input>
+      <div className={css.BodyInfoContainer}>
+        <h2 className={css.bodyTitle}>Body parameters</h2>
+        <p className={css.bodyText}>
+          Enter your parameters for correct performance tracking
+        </p>
+        <form className={css.bodyForm} onSubmit={formSubmit}>
+          <label htmlFor="user_height" className={css.bodyFormText}>
+            Height
+          </label>
+          <input
+            className={css.bodyFormInput}
+            id="user_height"
+            type="number"
+            name="height"
+            value={height}
+            onChange={handleChangeHeight}
+            placeholder="Enter your height"
+          ></input>
+          <label htmlFor="user_weight" className={css.bodyFormText}>
+            Weight
+          </label>
+          <input
+            className={css.bodyFormInput}
+            id="user_weight"
+            type="number"
+            name="weight"
+            value={weight}
+            onChange={handleChangeWeight}
+            placeholder="Enter your weight"
+          ></input>
 
-        <button className={css.bodyButton} type="submit">
-          Next
-        </button>
-      </form>
+          <button className={css.bodyButton} type="submit">
+            Next
+          </button>
+        </form>
 
-      <Link to="/usergender" className={css.bodyLinkBack}>
-        Back
-      </Link>
+        <Link to="/usergender" className={css.bodyLinkBack}>
+          Back
+        </Link>
+      </div>
     </div>
   );
 };
