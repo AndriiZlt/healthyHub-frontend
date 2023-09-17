79e158c89215c1b73b0b68cc3020d699cc52367d
Oleh Linskyi <118620479+OlehLinskyy@users.noreply.github.com>
2023-09-17 22:29:54 +0300

    new layout

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,16 +3,17 @@ import React from 'react';
 import css from './Header.module.css';
 import Logo from './Logo/Logo';
 import Navigation from './Navigation/Navigation';
-import Action from './Action/Action';
-import loseFat from '../../assets/Lose_fat.png';
-import waight from '../../assets/Waight_image.png';
+// import Action from './Action/Action';
+// import loseFat from '../../assets/Lose_fat.png';
+// import waight from '../../assets/Waight_image.png';
 import useMediaQuery from 'hooks/useMediaQuery';
-
-
+import Dropdown from 'react-bootstrap/Dropdown';
+import ButtonDropDown from './ButtonDropDown/ButtonDropDown';
+import menu from '../../assets/menu.svg';
 
 const Header = () => {
   const isMobile = useMediaQuery('(max-width:833px)');
-  const isLoggedIn = false;
+  const isLoggedIn = true;
 
   return (
     <div className={css.header}>
@@ -26,6 +27,53 @@ const Header = () => {
           <Logo />
           <div className={css.header_action}>
             {isMobile ? (
+              <>
+                <Dropdown>
+                  <Dropdown.Toggle className={css.button}>
+                    <img src={menu} alt="menu" />
+                  </Dropdown.Toggle>
+                  <Dropdown.Menu>TADAM</Dropdown.Menu>
+                </Dropdown>
+              </>
+            ) : (
+              <>
+                <Dropdown>
+                  <Dropdown.Toggle className={css.button}>
+                    <ButtonDropDown
+                      image="https://i.ibb.co/T8wdLSc/Lose-fat.png"
+                      title="Goal"
+                      text="Lose fat"
+                    />
+                  </Dropdown.Toggle>
+                  <Dropdown.Menu>TADAM</Dropdown.Menu>
+                </Dropdown>
+                <Dropdown>
+                  <Dropdown.Toggle className={css.button}>
+                    <ButtonDropDown
+                      image="https://i.ibb.co/y5LpgvL/Waight-image.png"
+                      title="Weight"
+                      text="65"
+                      subtext="kg"
+                      editIcon={true}
+                    />
+                  </Dropdown.Toggle>
+                  <Dropdown.Menu>TADAM</Dropdown.Menu>
+                </Dropdown>
+              </>
+            )}
+
+            <Dropdown align="end">
+              <Dropdown.Toggle className={css.button}>
+                <ButtonDropDown
+                  image="https://i.ibb.co/Qj72cs1/among-us.png"
+                  text="Konstantin"
+                  userAction={true}
+                />
+              </Dropdown.Toggle>
+              <Dropdown.Menu>TADAM</Dropdown.Menu>
+            </Dropdown>
+
+            {/* {isMobile ? (
               <Action
                 isMobile={true}
                 child={
@@ -50,13 +98,13 @@ const Header = () => {
                   editIcon={true}
                 />
               </>
-            )}
+            )} */}
 
-            <Action
+            {/* <Action
               text="Konstantin"
               userAction={true}
               image="https://i.ibb.co/Qj72cs1/among-us.png"
-            />
+            /> */}
           </div>
         </>
       )}
