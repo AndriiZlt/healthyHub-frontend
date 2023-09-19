cc1cfe4aa796f6cec30974cfa68cc7812651138e
Oleh Linskyi <118620479+OlehLinskyy@users.noreply.github.com>
2023-09-19 21:51:09 +0300

    add

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -10,21 +10,24 @@ import menu from '../../assets/menu.svg';
 import authSelectors from 'redux/auth/auth-selectors';
 import MobileAction from './MobileAction/MobileAction';
 import LogoutModal from './LogoutModal/LogoutModal';
-import Target from 'components/Target/SelectTarget';
+// import Target from 'components/Target/SelectTarget';
 
 const Header = () => {
   const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
 
-  const data = useSelector(authSelectors.getRegData);
-  console.log(data);
+  const {avatarURL, name, goal, weight} = useSelector(authSelectors.getUser);
   const isMobile = useMediaQuery('(max-width:833px)');
 
   const [showDropdown, setShowDropdown] = useState(false);
+  const [showSetting, setShowSetting] = useState(false);
   function closeButton() {
     setShowDropdown(false);
   }
   function openButton() {
     setShowDropdown(true);
+  }
+  function closeModal() {
+    setShowSetting(false)
   }
   return (
     <div className={css.header}>
@@ -35,12 +38,15 @@ const Header = () => {
         </>
       ) : (
         <>
-          <Dropdown show={showDropdown} onToggle={e => setShowDropdown(e)}>
-            <Dropdown.Toggle className={css.button} />
-            <Dropdown.Menu className={`${css.modal} ${css.mobile_modal}`}>
-              <MobileAction closeButton={closeButton} />
-            </Dropdown.Menu>
-          </Dropdown>
+          {isMobile && (
+            <Dropdown show={showDropdown} onToggle={e => setShowDropdown(e)}>
+              <Dropdown.Toggle className={css.button} />
+              <Dropdown.Menu className={`${css.modal} ${css.mobile_modal}`}>
+                <MobileAction closeButton={closeButton} />
+              </Dropdown.Menu>
+            </Dropdown>
+          )}
+
           <Logo />
           <div className={css.header_action}>
             {isMobile ? (
@@ -56,11 +62,11 @@ const Header = () => {
                     <ButtonDropDown
                       image="https://i.ibb.co/T8wdLSc/Lose-fat.png"
                       title="Goal"
-                      text="Lose fat"
+                      text={goal}
                     />
                   </Dropdown.Toggle>
                   <Dropdown.Menu className={css.modal}>
-                    <Target />
+                    TADAM
                   </Dropdown.Menu>
                 </Dropdown>
                 <Dropdown>
@@ -68,7 +74,7 @@ const Header = () => {
                     <ButtonDropDown
                       image="https://i.ibb.co/y5LpgvL/Waight-image.png"
                       title="Weight"
-                      text="65"
+                      text={weight}
                       subtext="kg"
                       editIcon={true}
                     />
@@ -78,16 +84,16 @@ const Header = () => {
               </>
             )}
 
-            <Dropdown align="end">
+            <Dropdown show={showSetting} onToggle={e => setShowSetting(e)} align="end">
               <Dropdown.Toggle className={css.button}>
                 <ButtonDropDown
-                  image="https://i.ibb.co/Qj72cs1/among-us.png"
-                  text="Konstantin"
+                  image={avatarURL}
+                  text={name}
                   userAction={true}
                 />
               </Dropdown.Toggle>
               <Dropdown.Menu className={css.modal}>
-                <LogoutModal />
+                <LogoutModal closeModal={closeModal}/>
               </Dropdown.Menu>
             </Dropdown>
           </div>
