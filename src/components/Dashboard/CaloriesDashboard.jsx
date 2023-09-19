39281dab01110f16678f8933f1126ab7ee63b289
Yaroslav <sych521@gmail.com>
2023-09-19 20:37:59 +0300

    chart.js added

--- src/components/Dashboard/CaloriesDashboard.jsx
+++ src/components/Dashboard/CaloriesDashboard.jsx
@@ -1,29 +1,104 @@
-import React from 'react';
-import Chart from 'chart.js/auto';
+import React, { useState } from 'react';
+import { Line } from 'react-chartjs-2';
+import { Chart as ChartJS } from 'chart.js/auto'; // eslint-disable-line no-unused-vars
+import css from "./Dashboard.module.css";
 
-const CaloriesDashboard = () => {
-	// const ctx = document.getElementById('myChart');
-	// new Chart(ctx, {
-	// 	type: 'bar',
-	// 	data: {
-	// 		labels: Utils.months({ count: 7 }),
-	// 		datasets: [{
-	// 			label: 'My First Dataset',
-	// 			data: [65, 59, 80, 81, 56, 55, 40],
-	// 			fill: false,
-	// 			borderColor: 'rgb(75, 192, 192)',
-	// 			tension: 0.1
-	// 		}]
-	// 	},
-	// 	options: {
-	// 		scales: {
-	// 			y: {
-	// 				beginAtZero: true
-	// 			}
-	// 		}
-	// 	}
-	// });
-	return <div id="myChart"></div>;
+
+
+const CaloriesDashboard = (props) => {
+
+	const time = props.time === 'month' ? 'month' : 'year'
+
+	const data = [
+		{
+			id: 1,
+			month: "Jan",
+			userGain: 80,
+		},
+		{
+			id: 2,
+			month: "Feb",
+			userGain: 69,
+		},
+		{
+			id: 3,
+			month: "Mar",
+			userGain: 59,
+		},
+		{
+			id: 4,
+			month: "Apr",
+			userGain: 65,
+		},
+		{
+			id: 5,
+			month: "May",
+			userGain: 80,
+		},
+		{
+			id: 6,
+			month: "June",
+			userGain: 69,
+		},
+		{
+			id: 7,
+			month: "July",
+			userGain: 59,
+		},
+		{
+			id: 8,
+			month: "Aug",
+			userGain: 65,
+		},
+		{
+			id: 9,
+			month: "Sep",
+			userGain: 80,
+		},
+		{
+			id: 10,
+			month: "Oct",
+			userGain: 69,
+		},
+		{
+			id: 11,
+			month: "Nov",
+			userGain: 59,
+		},
+		{
+			id: 12,
+			month: "Dec",
+			userGain: 65,
+		},
+	];
+
+	// for (let i = 0; i < props.numberOfDaysInMonth; i++) {
+	// 	data.push({
+	// 		id: i,
+	// 		month: "Dec",
+	// 		userGain: 65,
+	// 	});
+	// }
+
+	const [chartData] = useState({
+		labels: data.map(item => item.month),
+		datasets: [
+			{
+				label: 'Your Calories',
+				data: data.map(item => item.userGain),
+				backgroundColor: '#ff0d00',
+				borderColor: '#E3FFA8',
+				borderWidth: 1,
+				hoverOffset: 4,
+				tension: 0.3,
+			},
+		],
+	});
+	return (
+		<div className={css.dashboardContent}>
+			<Line data={chartData} />
+		</div>
+	);
 };
 
 export default CaloriesDashboard;
--- src/components/Dashboard/WeightDashboard.jsx
+++ src/components/Dashboard/WeightDashboard.jsx
@@ -1,8 +1,78 @@
 import React from 'react';
-import Chart from 'chart.js/auto';
+import css from "./Dashboard.module.css";
 
-const WeightDashboard = () => {
-	return <div></div>;
+const WeightDashboard = (props) => {
+
+	const days = [];
+	for (let i = 0; i < props.numberOfDaysInMonth; i++) {
+		days.push(<li key={i}>
+			<p className={css.weightDashboardKg}>{/* SERVER DATA, ЗАБРАТИ 70 */70}</p>
+			<p className={css.weightDashboardDate}>{i + 1}</p>
+		</li>);
+	}
+
+	const time = props.time
+
+	if (time === 'month') {
+		return <div className={css.WeightDashboardContainer}>
+			<ul className={css.weightDashboardList}>
+				{days}
+			</ul>
+		</div>;
+	} else {
+		return <div className={[css.dashboardContainer, css.WeightDashboardContainer]}>
+			<ul className={css.weightDashboardList} style={{ justifyContent: 'space-between', padding: '24px 24px 36px 24px' }}>
+				<li>
+					<p className={css.weightDashboardKg}>{/* SERVER DATA, ЗАБРАТИ 70 */70}</p>
+					<p className={css.weightDashboardDate}>January</p>
+				</li>
+				<li>
+					<p className={css.weightDashboardKg}>{/* SERVER DATA, ЗАБРАТИ 70 */70}</p>
+					<p className={css.weightDashboardDate}>February</p>
+				</li>
+				<li>
+					<p className={css.weightDashboardKg}>{/* SERVER DATA, ЗАБРАТИ 70 */70}</p>
+					<p className={css.weightDashboardDate}>March</p>
+				</li>
+				<li>
+					<p className={css.weightDashboardKg}>{/* SERVER DATA, ЗАБРАТИ 70 */70}</p>
+					<p className={css.weightDashboardDate}>April</p>
+				</li>
+				<li>
+					<p className={css.weightDashboardKg}>{/* SERVER DATA, ЗАБРАТИ 70 */70}</p>
+					<p className={css.weightDashboardDate}>May</p>
+				</li>
+				<li>
+					<p className={css.weightDashboardKg}>{/* SERVER DATA, ЗАБРАТИ 70 */70}</p>
+					<p className={css.weightDashboardDate}>June</p>
+				</li>
+				<li>
+					<p className={css.weightDashboardKg}>{/* SERVER DATA, ЗАБРАТИ 70 */70}</p>
+					<p className={css.weightDashboardDate}>July</p>
+				</li>
+				<li>
+					<p className={css.weightDashboardKg}>{/* SERVER DATA, ЗАБРАТИ 70 */70}</p>
+					<p className={css.weightDashboardDate}>August</p>
+				</li>
+				<li>
+					<p className={css.weightDashboardKg}>{/* SERVER DATA, ЗАБРАТИ 70 */70}</p>
+					<p className={css.weightDashboardDate}>September</p>
+				</li>
+				<li>
+					<p className={css.weightDashboardKg}>{/* SERVER DATA, ЗАБРАТИ 70 */70}</p>
+					<p className={css.weightDashboardDate}>October</p>
+				</li>
+				<li>
+					<p className={css.weightDashboardKg}>{/* SERVER DATA, ЗАБРАТИ 70 */70}</p>
+					<p className={css.weightDashboardDate}>November</p>
+				</li>
+				<li>
+					<p className={css.weightDashboardKg}>{/* SERVER DATA, ЗАБРАТИ 70 */70}</p>
+					<p className={css.weightDashboardDate}>December</p>
+				</li>
+			</ul>
+		</div>;
+	}
 };
 
 export default WeightDashboard;
