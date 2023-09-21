4e3fb31ac4e0dfa9dd4ea9412ff1b85e15c7ef98
AndriiZlt <andreyzlot@gmail.com>
2023-09-21 15:21:28 +0300

    test

--- src/redux/meals/meals-slice.js
+++ src/redux/meals/meals-slice.js
@@ -3,6 +3,8 @@ import mealsOperations from './meals-operations';
 
 const initialState = {
   today: null,
+  month: null,
+  yaer: null,
 };
 
 export const mealsSlice = createSlice({
@@ -10,9 +12,18 @@ export const mealsSlice = createSlice({
   initialState,
   reducers: {},
   extraReducers: builder => {
-    builder.addCase(mealsOperations.fetchDay.fulfilled, (state, action) => {
-      console.log('Fetch day fulfield', action.payload);
-      state.today = action.payload;
-    });
+    builder
+      .addCase(mealsOperations.fetchDay.fulfilled, (state, action) => {
+        console.log('Fetch day fulfield', action.payload);
+        state.today = action.payload;
+      })
+      .addCase(mealsOperations.fetchMonth.fulfilled, (state, action) => {
+        console.log('Fetch month fulfield', action.payload);
+        state.month = action.payload;
+      })
+      .addCase(mealsOperations.fetchYear.fulfilled, (state, action) => {
+        console.log('Fetch year fulfield', action.payload);
+        state.year = action.payload;
+      });
   },
 });
