545a4910f0e54792d217f525bbf13638f480c70b
k09at1ch <newwerasik228@gmail.com>
2023-09-23 21:22:36 +0300

    зроблено

--- src/components/Target/SelectTarget.jsx
+++ src/components/Target/SelectTarget.jsx
@@ -4,26 +4,45 @@ import style from './target.module.css';
 import Gainmuscle from './Gain muscle.png';
 import Losefat from './Lose fat image men.png';
 import Maintake from './Maintake image men.png';
+import cross from './close-circle.svg';
+import { useEffect } from 'react';
 
 function Target() {
   const [selected, setSelected] = useState(null);
+  const [selectedOption, setSelectedOption] = useState(null);
+
+  useEffect(() => {
+    const storedGoal = localStorage.getItem('userGoal');
+    if (storedGoal) {
+      setSelectedOption(storedGoal);
+    } else {
+      getGoal();
+    }
+  }, []);
+
+  const getGoal = async () => {
+    try {
+      const res = await axios.get(
+        'https://healthy-hub.onrender.com/api/user/current'
+      );
+      const userGoal = res.data.goal;
+      setSelectedOption(userGoal);
+      localStorage.setItem('userGoal', userGoal);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   const sendPatchRequest = async () => {
     try {
       const requestData = { goal: selected };
       const response = await axios.patch(
         'https://healthy-hub.onrender.com/api/user/change-goal',
-        requestData,
-        {
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        }
+        requestData
       );
-
-      console.log('Патч-запит успішно відправлено', response);
+      console.log(response);
     } catch (error) {
-      console.error('Помилка при відправленні патч-запиту', error);
+      console.error(error);
     }
   };
 
@@ -31,38 +50,104 @@ function Target() {
     sendPatchRequest();
   };
 
-  function selectedLoseFat() {
-    setSelected('lose_fat');
-  }
-
-  function selectedMaintain() {
-    setSelected('maintain');
-  }
-
-  function selectedGainmuscle() {
-    setSelected('gain_muscle');
+  function selectOption(option) {
+    setSelected(option);
   }
 
   return (
     <div className={style.targetContainer}>
-      <h1>Target selection</h1>
-      <h2>The service will adjust your calorie intake to your goal</h2>
-      <ul style={{ listStyle: 'none' }}>
+      <ul style={{ listStyle: 'none', display: 'flex', padding: 0, gap: 174 }}>
         <li>
-          <img src={Losefat} alt="Lose fat img" />
-
-          <button onClick={selectedLoseFat}>Lose fat</button>
+          <h1 className={style.title}>Target selection</h1>
         </li>
         <li>
-          <img src={Maintake} alt="Maintake img" />
-          <button onClick={selectedMaintain}>Maintain</button>
+          <img src={cross} alt="cross" style={{ cursor: 'pointer' }} />
+        </li>
+      </ul>
+      <h2 className={style.description}>
+        The service will adjust your calorie intake to your goal
+      </h2>
+      <ul
+        style={{
+          listStyle: 'none',
+          padding: 0,
+          display: 'flex',
+          flexDirection: 'column',
+          gap: '16px',
+          marginTop: '16px',
+          marginBottom: 0,
+        }}
+      >
+        <li>
+          <img
+            src={Losefat}
+            alt="Lose fat img"
+            onClick={() => {
+              selectOption('Lose fat');
+            }}
+            className={`${style.icon} ${
+              selected === 'Lose fat' ? style.selectedImage : selectedOption
+            }`}
+          />
+          <button
+            onClick={() => {
+              selectOption('Lose fat');
+            }}
+            className={`${style.button} ${
+              selected === 'Lose fat' ? style.selected : selectedOption
+            }`}
+          >
+            Lose fat
+          </button>
+        </li>
+        <li className={selected === 'maintain' ? style.selected : ''}>
+          <img
+            src={Maintake}
+            alt="Maintain img"
+            onClick={() => {
+              selectOption('Maintain');
+            }}
+            className={`${style.icon} ${
+              selected === 'Maintain' ? style.selectedImage : selectedOption
+            }`}
+          />
+          <button
+            onClick={() => {
+              selectOption('Maintain');
+            }}
+            className={`${style.button} ${
+              selected === 'Maintain' ? style.selected : selectedOption
+            }`}
+          >
+            Maintain
+          </button>
+        </li>
+        <li className={selected === 'Gain muscle' ? style.selected : ''}>
+          <img
+            onClick={() => {
+              selectOption('Gain muscle');
+            }}
+            className={`${style.icon} ${
+              selected === 'Gain muscle' ? style.selectedImage : selectedOption
+            }`}
+            src={Gainmuscle}
+            alt="Gainmuscle img"
+          />
+          <button
+            onClick={() => {
+              selectOption('Gain muscle');
+            }}
+            className={`${style.button} ${
+              selected === 'Gain muscle' ? style.selected : selectedOption
+            }`}
+          >
+            Gain muscle
+          </button>
         </li>
         <li>
-          <img src={Gainmuscle} alt="Gainmuscle img" />
-          <button onClick={selectedGainmuscle}>Gain muscle</button>
-        </li>
-        <li>
-          <button onClick={confirmSelection}>Confirm</button>
+          <button className={style.buttonconfirm} onClick={confirmSelection}>
+            Confirm
+          </button>
         </li>
       </ul>
     </div>
