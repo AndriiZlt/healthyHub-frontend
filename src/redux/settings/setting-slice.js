fd60862344683335c90e1c779a09201e22c2ec33
Iuliia23 <117689978+Iuliia23@users.noreply.github.com>
2023-09-24 02:52:02 +0200

    commit_1

--- src/redux/settings/setting-slice.js
+++ src/redux/settings/setting-slice.js
@@ -5,6 +5,7 @@ const initialState = {
   user: {
     name: null,
     gender: null,
+    // avatarURL: null,
     age: null,
     height: null,
     weight: null,
@@ -23,9 +24,12 @@ const settingsSlice = createSlice({
         state.user = action.payload;
       })
       .addCase(
-        settingsOperations.cancelSettings.fulfilled,
-        (state, _) => (state.user = initialState.user)
-      );
+        settingsOperations.cancelSettings.fulfilled,(state, _) => {
+          state.user = initialState.user;
+        })
+      .addCase(settingsOperations.updateAvatar.fulfilled, (state, action) => {
+        state.user.avatarURL = action.payload;
+      });
   },
 });
 
