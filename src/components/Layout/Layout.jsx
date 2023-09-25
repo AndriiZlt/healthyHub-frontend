d6e846e91789a1543367040c5808d4f8f3195376
AndriiZlt <andreyzlot@gmail.com>
2023-09-25 11:08:20 +0300

    Water chart update

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -18,7 +18,7 @@ const Layout = () => {
         <Container>
           <Suspense fallback={<div>Loading...</div>}>
             <Outlet />
-            <p style={{ color: '#2b2b28', marginTop: 30 }}>Group#4</p>
+            <p style={{ color: '#141414', marginTop: 30 }}>Group#4</p>
           </Suspense>
         </Container>
       </main>
